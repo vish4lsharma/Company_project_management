16f1f686021696395dd3c7a0b45c55da2df3f0e2
vish4lsharma <vishalsharma08555252@gmail.com>
2025-08-16 22:06:56 +0530

    Add employee-to-employee query and response system

--- backend/a.js
+++ backend/a.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 async function generateHash() {
-    const password = 'AdminPlacement@178';
+    const password = 'adminPlacement@178';
     const hash = await bcrypt.hash(password, 12);
     console.log('Password:', password);
     console.log('Hash:', hash);
